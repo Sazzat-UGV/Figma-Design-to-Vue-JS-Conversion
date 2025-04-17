18e518736d10259112e736725af3f372026ed4a5
Asikul Islam Sazzat <asikulislamsazzat@gmail.com>
2025-04-17 16:08:56 +0600

    Navbar design

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,5 +1,6 @@
 <template>
   <nav
+    ref="navbar"
     class="fixed top-0 left-0 w-full bg-primary-200 z-200 transition duration-200"
   >
     <div class="container flex items-center justify-between h-20">
@@ -44,7 +45,7 @@
         </button>
       </div>
       <div
-        class="absolute top-20 inset-x-0 flex flex-col lg:flex-row items-center gap-5 lg:gap-10 bg-primary-200 lg:bg-transparent pt-5 pb-10 px-5 lg:p-0 transition duration-500 lg:duration-200 lg:-translate-y-0"
+        class="absolute lg:static top-20 inset-x-0 flex flex-col lg:flex-row items-center gap-5 lg:gap-10 bg-primary-200 lg:bg-transparent pt-5 pb-10 px-5 lg:p-0 transition duration-500 lg:duration-200 lg:-translate-y-0"
         :class="!showMenu ? 'translate-y-[-150%]' : '-translate-y-0'"
       >
         <a href="" @click="showMenu = false" class="text-white font-medium"
@@ -59,6 +60,11 @@
         <a href="" @click="showMenu = false" class="text-white font-medium"
           >Trick Or Treat</a
         >
+        <button
+          class="hidden md:block px-8 py-3 rounded-full text-white font-medium tracking-wider uppercase bg-secondary w-full lg:w-auto"
+        >
+          Donate
+        </button>
       </div>
     </div>
   </nav>
@@ -67,4 +73,14 @@
 import { onMounted, ref } from "vue";
 const navbar = ref("");
 const showMenu = ref(false);
+
+onMounted(() => {
+  window.onscroll = () => {
+    if (window.scrollY == 0) {
+      navbar.value.classList.remove("bg-primary-200");
+    } else {
+      navbar.value.classList.add("bg-primary-200");
+    }
+  };
+});
 </script>
