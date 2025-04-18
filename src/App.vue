f826818e0ee12bced0086a7a67587fa55ee68481
Asikul Islam Sazzat <asikulislamsazzat@gmail.com>
2025-04-19 00:22:22 +0600

    Trick and Treat Section

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@
 <Navbar/>
 <Home/>
 <Category/>
+<TrickOrTreat/>
 </div>
 </template>
 
@@ -10,4 +11,5 @@
 import Navbar from './components/Navbar.vue';
 import Home from './components/Home.vue';
 import Category from './components/Cateogry.vue';
+import TrickOrTreat from './components/TrickOrTreat.vue';
 </script>
