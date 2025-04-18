c9f30138695010bdfa8f64daaaaca963cc9795c7
Asikul Islam Sazzat <asikulislamsazzat@gmail.com>
2025-04-18 23:07:44 +0600

    Category and About Section

--- src/components/Cateogry.vue
+++ src/components/Cateogry.vue
@@ -15,6 +15,26 @@
       </div>
     </div>
   </section>
+  <section
+    id="about"
+    class="grid grid-flow-col grid-cols-1 md:grid-cols-2 md:gap-10 items-center"
+  >
+    <div class="md:p-10">
+      <img src="../assets/images/category2-img.webp" alt="" class="w-full" />
+    </div>
+    <div class="text-center md:text-left flex flex-col items-center md:items-start ">
+      <h2 class="text-5xl md:text-6xl font-bold text-white ">What's that</h2>
+      <div class="flex items-center gap-5">
+        <h2 class="text-5xl md:text-6xl font-bold text-white">Halloween</h2>
+        <img src="../assets/images/head2-img.png" alt="" class="w-9 md:w-12" />
+      </div>
+      <p class="mt-8 md:mt-12 text-white text-lg md:text-xl w-full md:w-[80%]">
+        Halloween or Hallowe'en, also known as Allhalloween, All Hallows' Eve,
+        or All Saints' Eve, is a celebration observed in many countries on 31
+        October, the eve of the Western Christian feast of All Hallows' Day.
+      </p>
+    </div>
+  </section>
 </template>
 <script setup>
 import { ref } from "vue";
